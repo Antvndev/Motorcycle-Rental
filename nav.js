acde753f8751ff034f1a869ee89dc6572afb7506
Tony <202325235+Antvndev@users.noreply.github.com>
2025-04-22 11:16:19 +0300

    Added automatic closing of the navbar and fixed scroll isues

--- nav.js
+++ nav.js
@@ -1,6 +1,16 @@
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
+let navLinks = document.querySelectorAll('.navlist a');
+
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
 }
+
+// Close menu on link click (for mobile)
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('bx-x');
+        navlist.classList.remove('open');
+    });
+});
